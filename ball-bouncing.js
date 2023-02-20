944280309cab62c5eaeb470056877981097c2e4b
htseee <html.se@yandex.ru>
2023-02-20 22:12:45 +0700

    added ball-bouncing.js ellipse

--- ball-bouncing.js
+++ ball-bouncing.js
@@ -13,8 +13,6 @@ let b = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  // TODO: Create a for loop that iterates through i until it reaches the ball count value
   for (let i = 0; i < ballCount; i++) {
     x[i] = width / 2;
     y[i] = height / 2;    
@@ -25,20 +23,10 @@ function setup() {
     g[i] = random(256);
     b[i] = random(256);    
   }
-    // Inside the for loop:
-    // TODO: Set x and y position to be the center
-
-    // TODO: Set the speeds to be random
-
-    // TODO: Set the size to be random
-
-    // TODO: Set the colors to be random
 }
 
 function draw() {
   background(0, 50);
-  
-  // TODO: Iterate through a new for loop to change the properties in order to animate the balls
   for (let i = 0; i < ballCount; i++) {
     x[i] = x[i] + xSpeed[i];
     y[i] = y[i] + ySpeed[i];
@@ -52,23 +40,4 @@ function draw() {
     noStroke();
     ellipse(x[i], y[i], size[i], size[i]);
   }
-    // Inside the for loop:
-    // TODO: Increment speed for x position
-
-    // TODO: Increment speed for y position
-
-    
-    // TODO: Reverse x direction if ball hits left or right sides
-
-
-    // TODO: Reverse y direction if ball hits top or bottom sides
-
-
-    // TODO: Set random R, G, B values
-
-
-    // TODO: Style to have no strokes
-
-
-    // TODO: Draw the bouncing balls
 }
